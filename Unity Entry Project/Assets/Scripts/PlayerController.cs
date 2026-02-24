842feba225b0c860c3431bf0ae7391f73dc70476
bahadirdemirel <demirel.d2015@gmail.com>
2026-02-24 22:53:35 +0300

    Step2.13: Sound is added

--- Unity Entry Project/Assets/Scripts/PlayerController.cs
+++ Unity Entry Project/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,14 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 10f;
+    private AudioSource audioSource; // Ses bileþenini tutacak deðiþken
 
     void Start()
     {
         // Eðer Rigidbody atanmamýþsa otomatik olarak almayý dener
         if (rb == null) rb = GetComponent<Rigidbody>();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -22,5 +25,15 @@ public class PlayerController : MonoBehaviour
 
         // Kübe hareket yönünde kuvvet uygula
         rb.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+        // 'M' tuþuna basýldýðýnda sesi aç veya kapat
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            // Eðer sessizdeyse aç, açýksa sessize al (Tersine çevir)
+            audioSource.mute = !audioSource.mute;
+        }
     }
 }
